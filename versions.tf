85d80df9ad8ac06b64c0441e10ff5c7e6a302b30
boonhuilim73 <117626439+boonhuilim73@users.noreply.github.com>
2022-11-07 14:40:24 +0800

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -13,7 +13,7 @@ terraform {
   required_version = ">= 0.15"
 
   backend "remote" {
-    organization = "<YOUR_TERRAFORM_ORG>"
+    organization = "marketnode"
 
     workspaces {
       name = "sentinel-example"
